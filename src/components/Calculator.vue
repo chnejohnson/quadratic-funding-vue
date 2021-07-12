95c233d0e2e76b75a379f961cdb78b89b8fcdaff
Johnson Chen <chnejohnson@gmail.com>
2021-07-12 19:46:43 +0800

    feat: prototype completed

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex p-5">
     <div class="w-1/2 border-2 border-gray-100 p-5 mr-2">
-      <p>MATCH AMOUNT</p>
+      <p>TOTAL MATCH AMOUNT</p>
       <input
-        v-model="matchAmount"
+        v-model="totalMatch"
         type="number"
       />
     </div>
@@ -50,29 +50,34 @@
               <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Funding</span>
               <div>
                 <span
-                  v-for="(funding,j) in grant.funding"
+                  v-for="(fund,j) in grant.funding"
                   :key="j"
                 >
                   <tag
-                    :amount="funding"
+                    :amount="fund"
                     @remove="removeFunding(i, j)"
                   />
                 </span>
 
                 <input
-                  v-model="grant.fundingInput"
-                  @keyup.enter="addFunding(i, grant.fundingInput)"
+                  v-model="fundingInputs[i]"
+                  @keyup.enter="addFunding(i, fundingInputs[i])"
                   type="text"
                   class="inline-block w-1/2 appearance-none block bg-white text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
                   placeholder="Add a contribution and press Enter"
                 />
               </div>
             </td>
+            <!-- Funded Amount -->
             <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
               <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Funded Amount</span>
+              <p>{{ grant.fundingAmount }}</p>
             </td>
+
+            <!-- Match Amount -->
             <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
               <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Match Amount</span>
+              <p>{{ grant.match.toFixed(2) }}</p>
             </td>
           </tr>
         </tbody>
@@ -94,49 +99,83 @@
 </template>
 
 <script lang="ts">
-import { ref, computed, defineComponent } from "vue";
+import { ref, computed, defineComponent, watch } from "vue";
 import IconTrash from "./IconTrash.vue";
 import Tag from "./Tag.vue";
+
+interface Grant {
+  funding: number[];
+  fundingAmount: number;
+  match: number;
+}
 
 export default defineComponent({
   components: { IconTrash, Tag },
   setup() {
-    const matchAmount = ref(0);
-    const grants = ref([
-      { funding: [1], fundingAmount: 0, match: 0, fundingInput: "" },
-      { funding: [32, 32], fundingAmount: 0, match: 0, fundingInput: "" },
-      { funding: [], fundingAmount: 0, match: 0, fundingInput: "" },
-      { funding: [], fundingAmount: 0, match: 0, fundingInput: "" },
+    const totalMatch = ref(1000);
+    const grants = ref<Grant[]>([
+      { funding: [1, 1, 1], fundingAmount: 0, match: 0 },
+      { funding: [9], fundingAmount: 0, match: 0 },
+      { funding: [], fundingAmount: 0, match: 0 },
+      { funding: [], fundingAmount: 0, match: 0 },
     ]);
+    const fundingInputs = ref(["", "", "", ""]);
+    const grantsNum = computed(() => grants.value.length);
+
+    const calculateMatch = () => {
+      let sum = 0;
+      grants.value.forEach((grant) => {
+        let rootSum = 0;
+        let fundingAmount = 0;
+        grant.funding.forEach((fund) => {
+          fundingAmount += fund;
+          rootSum += Math.sqrt(fund);
+        });
+        grant.fundingAmount = fundingAmount;
+        grant.match = rootSum * rootSum;
+        sum += grant.match;
+      });
+
+      grants.value.forEach((grant) => {
+        grant.match *= totalMatch.value / sum;
+      });
+    };
+
+    calculateMatch();
+
+    watch(totalMatch, () => {
+      calculateMatch();
+    });
 
     const removeFunding = (grantIndex: number, fundingIndex: number) => {
       grants.value[grantIndex].funding = grants.value[
         grantIndex
       ].funding.filter((_, i) => i !== fundingIndex);
+      calculateMatch();
     };
 
     const addFunding = (grantIndex: number, fundingInput: string) => {
       if (Number(fundingInput) === NaN || !Number(fundingInput)) {
-        grants.value[grantIndex].fundingInput = "";
+        fundingInputs.value[grantIndex] = "";
         return;
       }
       grants.value[grantIndex].funding.push(Number(fundingInput));
-      grants.value[grantIndex].fundingInput = "";
+      fundingInputs.value[grantIndex] = "";
+      calculateMatch();
     };
-
-    const grantsNum = computed(() => grants.value.length);
 
     const addGrant = () => {
       grants.value.push({
         funding: [],
         fundingAmount: 0,
         match: 0,
-        fundingInput: "",
       });
+      calculateMatch();
     };
 
     const removeGrant = (grantIndex: number) => {
       grants.value = grants.value.filter((g, i) => i !== grantIndex);
+      calculateMatch();
     };
 
     const copyURL = () => {};
@@ -144,12 +183,14 @@ export default defineComponent({
     return {
       grants,
       grantsNum,
-      matchAmount,
+      totalMatch,
+      fundingInputs,
       addGrant,
       copyURL,
       removeGrant,
       addFunding,
       removeFunding,
+      calculateMatch,
     };
   },
 });
